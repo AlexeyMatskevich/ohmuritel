f5cebaad7ef097eaa98ecc29c3376ab04efda5a7
AlexeyMatskevich <alexey.matskevich@flatstack.com>
2019-09-24 01:42:31 +0300

    Add spec for user query

---
+++ spec/graphql/types/query_type_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe Types::QueryType do
+  describe "users" do
+    let!(:users) { create_pair(:user) }
+
+    let(:query) do
+      %(query {
+        users {
+          email
+        }
+      })
+    end
+
+    subject(:result) do
+      OhmuritelSchema.execute(query).as_json
+    end
+
+    it "returns all users" do
+      expect(result.dig("data", "users")).to match_array(
+        users.map { |user| {"email" => user.email} }
+      )
+    end
+  end
+end
