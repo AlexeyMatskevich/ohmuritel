95ca36a474e8728d702096beff670c29fcf3885a
AlexeyMatskevich <alexey.matskevich@flatstack.com>
2019-09-24 00:17:02 +0300

    Add user graph query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,12 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :users,
+      [Types::UserType],
+      null: false,
+      description: "Returns a list of users in the martian library"
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def users
+      User.all
     end
   end
 end
