32c1b7535432d92eb63c9aea55e116af00fce59d
AlexeyMatskevich <alexey.matskevich@flatstack.com>
2019-09-23 23:28:57 +0300

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  [
+    {
+      email: "john.doe@example.com",
+      first_name: "John",
+      last_name: "Doe"
+    },
+    {
+      email: "jane.doe@example.com",
+      first_name: "Jane",
+      last_name: "Doe"
+    }
+  ]
+)
