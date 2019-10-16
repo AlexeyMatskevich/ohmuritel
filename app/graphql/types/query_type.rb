abac447041cd7beec774845865a20fb9d076cbbc
AlexeyMatskevich <alexey.matskevich@flatstack.com>
2019-10-17 01:23:35 +0300

    Install gem for fix N+1
    * Add frontend package for from and date in frontend
    * Improve development environment

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,10 +3,19 @@ module Types
     field :users,
       [Types::UserType],
       null: false,
-      description: "Returns a list of users in the martian library"
+      description: "Returns a list of users"
+
+    field :user, Types::UserType, null: false do
+      description "Returns a user"
+      argument :id, ID, required: true
+    end
 
     def users
       User.all
+    end
+
+    def user(id:)
+      User.find(id)
     end
   end
 end
