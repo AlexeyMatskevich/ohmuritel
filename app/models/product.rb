9c06c9bf59d264df4ef8159915159cb947725213
AlexeyMatskevich <alexey.matskevich@flatstack.com>
2019-11-15 19:55:43 +0300

    Add image to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_rich_text :description
+  has_one_attached :image
 
   validates :name, presence: true, length: {maximum: 55}, uniqueness: {case_sensitive: false}
   validates :weight, presence: true, numericality: {only_integer: true, greater_than: 0}
