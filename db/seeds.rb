b5fb26c94356aecf04f6e50e4687ce7f33405f13
AlexeyMatskevich <alexey.matskevich@flatstack.com>
2019-11-18 16:08:18 +0300

    Apply lint offenses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-include ActionDispatch::TestProcess
 PREVIEW_DESCRIPTION = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
 DESCRIPTION = "<div class=\"trix-content\">\n  <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1><div>Ut enim ad minim <strong>veniam</strong>, quis nostrud exercitation ullamco laboris <strong>nisi</strong> ut aliquip ex ea <em>commodo consequat</em>. Duis aute irure dolor in <del>reprehenderit</del> in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><br>\n</div><ol>\n<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>\n<li>Aliquam tincidunt mauris eu risus.</li>\n<li>Vestibulum auctor dapibus neque.<ul><li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li></ul>\n</li>\n</ol><ul>\n<li>Aliquam tincidunt mauris eu risus.</li>\n<li>Vestibulum auctor dapibus neque.</li>\n</ul><pre> Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n    Aliquam tincidunt mauris eu risus.\n    Vestibulum auctor dapibus neque.</pre><h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1><div>Ut enim ad minim <strong>veniam</strong>, quis nostrud exercitation ullamco laboris <strong>nisi</strong> ut aliquip ex ea <em>commodo consequat</em>. Duis aute irure dolor in <del>reprehenderit</del> in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><br>\n</div><ol>\n<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>\n<li>Aliquam tincidunt mauris eu risus.</li>\n<li>Vestibulum auctor dapibus neque.<ul><li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li></ul>\n</li>\n</ol><ul>\n<li>Aliquam tincidunt mauris eu risus.</li>\n<li>Vestibulum auctor dapibus neque.</li>\n</ul><pre> Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n    Aliquam tincidunt mauris eu risus.\n    Vestibulum auctor dapibus neque.</pre>\n</div>\n"
-
-BURGER = fixture_file_upload(Rails.root.join("public", "burger.jpg"), "image/jpg")
-
-CHEESEBURGER = fixture_file_upload(Rails.root.join("public", "cheeseburger.jpg"), "image/jpg")
 
 User.create!(
   [
@@ -38,24 +33,24 @@ User.create!(
   ]
 )
 
+burger = Product.create(
+  name: "Burger",
+  weight: 40,
+  price: 20,
+  preview_description: PREVIEW_DESCRIPTION,
+  description: DESCRIPTION
+)
+burger.image.attach(io: File.open("public/burger.jpg"), filename: "burger.jpg", content_type: "image/jpg")
+cheeseburger = Product.create(
+  name: "Cheeseburger",
+  weight: 40,
+  price: 30,
+  preview_description: PREVIEW_DESCRIPTION,
+  description: DESCRIPTION
+)
+cheeseburger.image.attach(io: File.open("public/cheeseburger.jpg"), filename: "cheeseburger.jpg", content_type: "image/jpg")
 Product.create!(
   [
-    {
-      name: "Burger",
-      weight: 40,
-      price: 20,
-      preview_description: PREVIEW_DESCRIPTION,
-      description: DESCRIPTION,
-      image: BURGER,
-    },
-    {
-      name: "Cheeseburger",
-      weight: 40,
-      price: 30,
-      preview_description: PREVIEW_DESCRIPTION,
-      description: DESCRIPTION,
-      image: CHEESEBURGER,
-    },
     {
       name: "Echpochmak",
       weight: 40,
