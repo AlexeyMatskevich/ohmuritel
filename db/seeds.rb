961037a834acdccd34262b8cb08759936c488781
AlexeyMatskevich <alexey.matskevich@flatstack.com>
2019-11-20 22:43:25 +0300

    Update seeds
    * Add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-PREVIEW_DESCRIPTION = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-
 DESCRIPTION = "<div class=\"trix-content\">\n  <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1><div>Ut enim ad minim <strong>veniam</strong>, quis nostrud exercitation ullamco laboris <strong>nisi</strong> ut aliquip ex ea <em>commodo consequat</em>. Duis aute irure dolor in <del>reprehenderit</del> in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><br>\n</div><ol>\n<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>\n<li>Aliquam tincidunt mauris eu risus.</li>\n<li>Vestibulum auctor dapibus neque.<ul><li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li></ul>\n</li>\n</ol><ul>\n<li>Aliquam tincidunt mauris eu risus.</li>\n<li>Vestibulum auctor dapibus neque.</li>\n</ul><pre> Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n    Aliquam tincidunt mauris eu risus.\n    Vestibulum auctor dapibus neque.</pre><h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1><div>Ut enim ad minim <strong>veniam</strong>, quis nostrud exercitation ullamco laboris <strong>nisi</strong> ut aliquip ex ea <em>commodo consequat</em>. Duis aute irure dolor in <del>reprehenderit</del> in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><br>\n</div><ol>\n<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>\n<li>Aliquam tincidunt mauris eu risus.</li>\n<li>Vestibulum auctor dapibus neque.<ul><li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li></ul>\n</li>\n</ol><ul>\n<li>Aliquam tincidunt mauris eu risus.</li>\n<li>Vestibulum auctor dapibus neque.</li>\n</ul><pre> Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n    Aliquam tincidunt mauris eu risus.\n    Vestibulum auctor dapibus neque.</pre>\n</div>\n"
 
 User.create!(
@@ -37,7 +35,7 @@ burger = Product.create(
   name: "Burger",
   weight: 40,
   price: 20,
-  preview_description: PREVIEW_DESCRIPTION,
+  preview_description: Faker::Food.description,
   description: DESCRIPTION
 )
 burger.image.attach(io: File.open("public/burger.jpg"), filename: "burger.jpg", content_type: "image/jpg")
@@ -45,53 +43,17 @@ cheeseburger = Product.create(
   name: "Cheeseburger",
   weight: 40,
   price: 30,
-  preview_description: PREVIEW_DESCRIPTION,
+  preview_description: Faker::Food.description,
   description: DESCRIPTION
 )
 cheeseburger.image.attach(io: File.open("public/cheeseburger.jpg"), filename: "cheeseburger.jpg", content_type: "image/jpg")
-Product.create!(
-  [
-    {
-      name: "Echpochmak",
-      weight: 40,
-      price: 25,
-      preview_description: PREVIEW_DESCRIPTION,
-      description: DESCRIPTION,
-    },
-    {
-      name: "Bun with cottage cheese",
-      weight: 40,
-      price: 40,
-      preview_description: PREVIEW_DESCRIPTION,
-      description: DESCRIPTION,
-    },
-    {
-      name: "Bun with meat",
-      weight: 40,
-      price: 14,
-      preview_description: PREVIEW_DESCRIPTION,
-      description: DESCRIPTION,
-    },
-    {
-      name: "Bun with strawberries",
-      weight: 40,
-      price: 5,
-      preview_description: PREVIEW_DESCRIPTION,
-      description: DESCRIPTION,
-    },
-    {
-      name: "Muffin roll",
-      weight: 40,
-      price: 2,
-      preview_description: PREVIEW_DESCRIPTION,
-      description: DESCRIPTION,
-    },
-    {
-      name: "bread",
-      weight: 40,
-      price: 3,
-      preview_description: PREVIEW_DESCRIPTION,
-      description: DESCRIPTION,
-    },
-  ]
-)
+
+100.times do
+  Product.create(
+    name: Faker::Food.dish,
+    weight: rand(10..150),
+    price: rand(1..15),
+    preview_description: Faker::Food.description,
+    description: Faker::Food.description,
+  )
+end
